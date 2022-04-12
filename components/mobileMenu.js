42744dcca711bdfd170a268d68de003c4f48d2f3
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-04-12 12:21:13 +0800

    feat: create MobileMenu for responsive navbar

---
+++ components/mobileMenu.js
@@ -0,0 +1,60 @@
+import {
+  Button,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { useRef } from "react";
+import Link from "next/link";
+
+export default function MobileMenu() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = useRef();
+
+  return (
+    <>
+      <Button ref={btnRef} onClick={onOpen}>
+        <HamburgerIcon />
+      </Button>
+      <Drawer
+        isOpen={isOpen}
+        placement="right"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        size={"xs"}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>Menu</DrawerHeader>
+          <DrawerBody>
+            <VStack>
+              <Link href={"/"}>
+                <a>Home</a>
+              </Link>
+              <Link href={"/projects"}>
+                <a>Projects</a>
+              </Link>
+              <Link href={"/posts"}>
+                <a>Posts</a>
+              </Link>
+            </VStack>
+            <style jsx>
+              {`
+                a:hover {
+                  text-decoration: underline;
+                }
+              `}
+            </style>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}

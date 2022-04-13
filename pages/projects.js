0e73ba593eea4b0136cc1253befca5ca2fc1320b
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-04-13 11:29:00 +0800

    feat(projects): add props to BlogPostWithImage components

--- pages/projects.js
+++ pages/projects.js
@@ -4,7 +4,22 @@ import BlogPostWithImage from "../components/cards";
 export default function projects() {
   return (
     <Layout>
-      <BlogPostWithImage />
+      <BlogPostWithImage
+        imageLink={"/images/unsplash1.jpg"}
+        altImage={"Image 1"}
+        heading={"CSS Illustration"}
+        description={"Create a Illustration using CSS"}
+        codeLink={"https://github.com/aroyan"}
+        previewLink={"https://aroyanbakti.com"}
+      />
+      <BlogPostWithImage
+        imageLink={"/images/unsplash1.jpg"}
+        altImage={"Image 2"}
+        heading={"CSS Illustration"}
+        description={"Create a Illustration using CSS"}
+        codeLink={"https://github.com/aroyan"}
+        previewLink={"https://aroyanbakti.com"}
+      />
     </Layout>
   );
 }

7c620690cd44dec9fc47dc8ba7649db32726f17f
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-04-15 22:56:31 +0800

    feat(projects): add new image for css chat app project card

--- pages/projects.js
+++ pages/projects.js
@@ -5,12 +5,12 @@ export default function projects() {
   return (
     <Layout>
       <BlogPostWithImage
-        imageLink={"/images/unsplash1.jpg"}
-        altImage={"Image 1"}
-        heading={"CSS Illustration"}
+        imageLink={"/images/chat-app-css.jpg"}
+        altImage={"Chat App CSS Illustration"}
+        heading={"Chat App CSS Illustration"}
         description={"Create a Illustration using CSS"}
-        codeLink={"https://github.com/aroyan"}
-        previewLink={"https://aroyanbakti.com"}
+        codeLink={"https://github.com/aroyan/chat-app"}
+        previewLink={"https://aroyan.github.io/chat-app"}
       />
       <BlogPostWithImage
         imageLink={"/images/unsplash1.jpg"}

09906cb9611ef2f297c6ffd19adff83338c932d8
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-04-16 22:41:17 +0800

    feat(head): add custom head and add meta tags

---
+++ components/layout/CustomHead.js
@@ -0,0 +1,49 @@
+import Head from "next/head";
+
+export default function CustomHead({ title }) {
+  return (
+    <Head>
+      <meta charset="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Aroyan Bakti | {title}</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta
+        name="title"
+        content="Aroyan Bakti Setiawan - I'm a Front End Developer based in Indonesia"
+      />
+      <meta
+        name="description"
+        content="Aroyan Bakti Setiawan Portfolio website, built with NextJS and ChakraUI"
+      />
+      <meta property="og:type" content="website" />
+      <meta
+        property="og:url"
+        content="https://portfolio-v2-arbase.vercel.app/"
+      />
+      <meta
+        property="og:title"
+        content="Aroyan Bakti Setiawan - I'm a Front End Developer based in Indonesia"
+      />
+      <meta
+        property="og:description"
+        content="Aroyan Bakti Setiawan Portfolio website, built with NextJS and ChakraUI"
+      />
+      <meta property="og:image" content="" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:url"
+        content="https://portfolio-v2-arbase.vercel.app/"
+      />
+      <meta
+        property="twitter:title"
+        content="Aroyan Bakti Setiawan - I'm a Front End Developer based in Indonesia"
+      />
+      <meta
+        property="twitter:description"
+        content="Aroyan Bakti Setiawan Portfolio website, built with NextJS and ChakraUI"
+      />
+      <meta property="twitter:image" content="" />
+    </Head>
+  );
+}

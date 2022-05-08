43049ad266583696bdd6e154689d25bcaf588040
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-05-08 13:44:43 +0800

    feat: :mag: pass props to fill meta tags dynamicly

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -2,11 +2,11 @@ import { Box, Container, useColorModeValue } from "@chakra-ui/react";
 import CustomHead from "./headMeta";
 import Navbar from "../navbar";
 
-export default function Layout({ children, title }) {
+export default function Layout({ children, title, content, card }) {
   const BG = useColorModeValue("#fdf6e3", "#002b36");
   return (
     <>
-      <CustomHead title={title} />
+      <CustomHead title={title} content={content} card={card} />
       <Box bg={BG} maxW={"full"} as={"main"}>
         <Navbar />
         <Container

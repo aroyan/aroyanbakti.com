0500bff1b7a547c3ae17726f38c9275e262fff17
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-05-30 10:37:41 +0800

    feat: :lipstick: update bg nav color to gray.900 and add position sticky

--- components/navbar.js
+++ components/navbar.js
@@ -5,9 +5,17 @@ import ToggleDarkMode from "./toggle-button";
 import DesktopMenu from "./desktopMenu";
 
 export default function Navbar() {
-  const navBg = useColorModeValue("#eee8d5", "#073642");
+  const navBg = useColorModeValue("#eee8d5", "gray.900");
   return (
-    <Container maxW={"full"} bg={navBg} p={"0"}>
+    <Container
+      maxW={"full"}
+      bg={navBg}
+      p={"0"}
+      boxShadow={"md"}
+      position={"sticky"}
+      top={"0"}
+      zIndex={"2"}
+    >
       <Container maxW={{ base: "full", md: "container.md" }} p={"0"}>
         <Flex p={"4"} align={"center"} justifyContent={"space-between"}>
           <Logo />

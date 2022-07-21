a7c86640db4b5335207af3725ff2018bf85ecb76
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-07-21 23:37:53 +0800

    fix: add return on sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -2,13 +2,11 @@ import React from "react";
 import fs from "fs";
 import { createClient } from "contentful";
 
-const Sitemap = () => {};
+const Sitemap = () => {
+  return null;
+};
 
 export const getServerSideProps = async ({ res }) => {
-  // const baseUrl = {
-  //   development: "http://localhost:3000",
-  //   production: "https://www.aroyanbakti.com",
-  // }[process.env.NODE_ENV];
   const baseUrl = "https://www.aroyanbakti.com";
 
   const staticPages = fs
@@ -64,7 +62,6 @@ export const getServerSideProps = async ({ res }) => {
                 <url>
                     <loc>${url}</loc>
                     <lastmod>${new Date().toISOString()}</lastmod>
-                    <changefreq>monthly</changefreq>
                     <priority>1.0</priority>
                 </url>
             `;

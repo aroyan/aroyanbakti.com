8c9cb1956abcdc139effcac0e97b9da05620278f
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-08-20 23:18:10 +0800

    wip

---
+++ components/Layout/BlogLayout.js
@@ -0,0 +1,32 @@
+import {
+  Box,
+  Center,
+  Grid,
+  Heading,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import CustomHead from "./HeadMeta";
+import Navbar from "../Navbar";
+
+export default function BlogLayout({ children, title }) {
+  const BG = useColorModeValue("#fdf6e3", "#1a202c");
+  return (
+    <>
+      <CustomHead title={title} />
+      <Box bg={BG} maxW={"full"} as={"main"}>
+        <Navbar />
+        <Center pb={"8"}>
+          <Grid
+            templateColumns={{ md: "repeat(2, 1fr)" }}
+            gap={"8"}
+            width={"container.md"}
+            justifyContent={"center"}
+            margin={{ base: "8" }}
+          >
+            {children}
+          </Grid>
+        </Center>
+      </Box>
+    </>
+  );
+}

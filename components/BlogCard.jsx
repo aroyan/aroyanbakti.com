c6546136b560ba43e648f8e1235b6c982eda3d63
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-08-20 22:25:22 +0800

    refactor: migrate blog from contentful to mdx

---
+++ components/BlogCard.jsx
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import {
+  Badge,
+  HStack,
+  Box,
+  Heading,
+  Text,
+  LinkBox,
+  LinkOverlay,
+  Flex,
+} from "@chakra-ui/react";
+
+const BlogCard = ({ meta }) => {
+  const { title, slug, excerpt, date, tags } = meta;
+  const [_, month, dt, year, ...rest] = new Date(date).toString().split(" ");
+
+  return (
+    <LinkBox
+      border="1px dashed gray"
+      rounded="md"
+      p="4"
+      _hover={{ border: "1px solid white" }}
+      display="flex"
+      flexDir="column"
+      gap="4"
+      justifyContent="space-between"
+    >
+      <Link href={`/blog/${slug}`} passHref>
+        <LinkOverlay fontWeight="bold" fontSize="1.15rem" color="orange.400">
+          {title}
+        </LinkOverlay>
+      </Link>
+      <Text>{excerpt}</Text>
+      <Flex wrap={"wrap"} gap="1">
+        {tags.map((tag) => (
+          <Badge key={tag} colorScheme="cyan">
+            <Link href={`tags/${tag}`}>{tag}</Link>
+          </Badge>
+        ))}
+      </Flex>
+      <Text>{`${dt} ${month} ${year}`}</Text>
+    </LinkBox>
+  );
+};
+
+export default BlogCard;
---
+++ pages/tags/[slug].js
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Layout from "../../components/Layout";
+import BlogLayout from "../../components/Layout/BlogLayout";
+import BlogCard from "../../components/BlogCard";
+import { getAllPosts } from "../../utils/api";
+import { Heading } from "@chakra-ui/react";
+
+export default function Tags({ slug, meta }) {
+  console.log(meta);
+  return (
+    <>
+      <BlogLayout>
+        {meta.map((x, id) => (
+          <BlogCard key={id} meta={x} />
+        ))}
+      </BlogLayout>
+    </>
+  );
+}
+
+export const getStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const posts = getAllPosts().filter((post) => post.meta.tags.includes(slug));
+
+  return {
+    props: {
+      slug,
+      meta: posts.map((post) => post.meta),
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const posts = getAllPosts();
+  const tags = new Set(posts.map((post) => post.meta.tags).flat());
+  const paths = Array.from(tags).map((tag) => ({ params: { slug: tag } }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
